16e33068391a69051909c89fa876d14dbb62edb7
Aneta <a.j.lisowska@gmail.com>
2021-04-21 22:47:34 +0200

    added motivation ability and trigger to patient environment

--- environment/fogg_behavioural_model.py
+++ environment/fogg_behavioural_model.py
@@ -1,17 +1,32 @@
 from abc import ABC
 
 from gym import Env
-#habituation to prompts (after appox 3 weeks)
+import numpy as np
+
+
+# habituation to prompts (after appox 3 weeks)
 # one episode is one day
 
 
 class Patient(Env):
 
-    def __init__(self, behaviour_threshold: int):
+    def __init__(self, behaviour_threshold: int, has_family: bool):
         self.behaviour_threshold = behaviour_threshold
+        self.has_family = has_family
+        self.reset()
 
     def reset(self):
-        pass
+        self.number_of_hours_slept_last_night = np.random.randint(3, 10)
+        self.sufficient_sleep = 1 if self.number_of_hours_slept_last_night > 7 else 0
+        self.valence = np.random.randint(0, 2)  # 0 negative, 1 positive
+        self.last_activity_score = np.random.randint(0, 2)  # 0 negative, 1 positive
+        self.arousal = np.random.randint(0, 2)  # 0 low, 1 high
+        self.cognitive_load = np.random.randint(0, 2)  # 0 low, 1 high
+        self.time_of_the_day = np.random.randint(0, 25)
+        self.day_of_the_week = np.random.randint(0, 8)  # 0 Monday, 7 Sunday
+        self.location = 'home' if 1 < self.time_of_the_day < 7 else np.random.choice(['home', 'other'])
+        self.activity = 'sleeping' if 1 < self.time_of_the_day < 5 else \
+            np.random.choice(['stationary', 'walking', 'driving', 'sleeping'])
 
     def step(self, action):
         pass
@@ -23,85 +38,76 @@ class Patient(Env):
         behaviour = motivation * ability * trigger
         return behaviour > self.behaviour_threshold
 
+    def get_motivation(self):
+        """
+        Factors impacting patient's motivation:
+        1) Jowsey et al (2014) "What motivates Australian health service users with chronic illness to engage in
+        self-management behaviour?"
 
+         - internal factors:  valence positive(+), negative (-)
+            "Remaining positive was one of the most important strategies many used for optimizing and
+             controlling their health"
+         - external factors: family (+), no family (-)
+         - demotivators: high past activity score (+),  low (-)
+            "perceiving self-management behaviour as having limited benefit"
 
+        2) Dolsen et al (2017) "Sleep the night before and after a treatment session: A critical ingredient for
+        treatment adherence?"
+        Axelsson et al (2020) "Sleepiness as motivation: a potential mechanism for how sleep deprivation affects behavior"
 
+        "sleepiness may be a central mechanism by which impaired alertness, for example, due to insufficient sleep,
+        contributes to poor quality of life and adverse health. We propose that sleepiness helps organize behaviors
+         toward the specific goal of assuring sufficient sleep, in competition with other needs and incentives" Axelsson
 
+         - hours of sleep the previous night, sufficient(+), insufficient(-)
 
+        """
+        return self.valence + self.has_family + self.last_activity_score + self.sufficient_sleep
 
+    def get_ability(self):
+        """"
+        1)Chan et al (2020) "Prompto: Investigating Receptivity to Prompts Based on Cognitive Load from Memory Training
+         Conversational Agent"
+         "users were more receptive to prompts and memory training under low cognitive load than under high cognitive load"
+        - cognitive load, high (-), low(+)
 
+        2) Goyal et al. (2017) users are likely to pay attention to the notifications at times of increasing arousal
+        - arousal, high (+), low (-)
 
+        3) Aminikhanghahi (2017) "Improving Smartphone Prompt Timing Through Activity Awareness"
+            "participants did not like to respond to AL queries when they were at work but were generally responsive
+             when they were relaxing " relaxing low cognitive load and positive valence
+             Ho et al. (2018). Location = [Home, Work, Other] , Motion activity = [Stationary, Walking, Running, Driving]
+         - home (+), other (-)
+         - stationary(+), walking(-), driving (-), sleeping (-)
 
-def get_motivation():
-    """
-    Factors impacting patient's motivation:
-    1) Jowsey et al (2014) "What motivates Australian health service users with chronic illness to engage in 
-    self-management behaviour?"
-    
-     - internal factors:  valence positive(+), negative (-)
-        "Remaining positive was one of the most important strategies many used for optimizing and
-         controlling their health"
-     - external factors: family (+), no family (-)
-     - demotivators: high past activity score (+),  low (-)
-        "perceiving self-management behaviour as having limited benefit"
-        
-    2) Dolsen et al (2017) "Sleep the night before and after a treatment session: A critical ingredient for 
-    treatment adherence?"
-    Axelsson et al (2020) "Sleepiness as motivation: a potential mechanism for how sleep deprivation affects behavior"
-    
-    "sleepiness may be a central mechanism by which impaired alertness, for example, due to insufficient sleep, 
-    contributes to poor quality of life and adverse health. We propose that sleepiness helps organize behaviors
-     toward the specific goal of assuring sufficient sleep, in competition with other needs and incentives" Axelsson 
-     
-     - hours of sleep the previous night, sufficient(+), insufficient(-)
-     
+        """
+        load = 1 if self.cognitive_load == 0 else 0
+        location = 1 if self.location == 'home' else 0
+        activity = 1 if self.activity == 'stationary' else 0
 
-    
-    
-    """
-    pass
+        return self.arousal + load + location + activity
 
+    def get_trigger(self):
+        """"
+        1)Bidargadi et al. (2018) "To Prompt or Not to Prompt? A Microrandomized Trial of Time-Varying Push Notifications to
+         Increase Proximal Engagement With a Mobile Health App"
+        Timing!: "users are more likely to engage with the app within 24 hours when push notifications are sent at mid-day
+         on weekends"
+         - time of the day
+         - day of the week
 
-def get_ability():
+        2) Akker etal (2015) "Tailored motivational message generation: A model and practical framework for real-time
+         physical activity coaching"
 
-    """"
-    1)Chan et al (2020) "Prompto: Investigating Receptivity to Prompts Based on Cognitive Load from Memory Training
-     Conversational Agent"
-     "users were more receptive to prompts and memory training under low cognitive load than under high cognitive load"
-    - cognitive load, high (-), low(+)
+        """
+        effective_prompt = True if 15 > self.time_of_the_day >= 11 or (self.day_of_the_week > 5 and
+                                                                       self.activity != 'sleeping') else False
 
-    2) Goyal et al. (2017) users are likely to pay attention to the notifications at times of increasing arousal
-    - arousal, high (+), low (-)
-
-    3) Aminikhanghahi (2017) "Improving Smartphone Prompt Timing Through Activity Awareness"
-        "participants did not like to respond to AL queries when they were at work but were generally responsive
-         when they were relaxing " relaxing low cognitive load and positive valence
-         Ho et al. (2018). Location = [Home, Work, Other] , Motion activity = [Stationary, Walking, Running, Driving]
-     - home (+), other (-)
-     - stationary(+), walking(-), driving (-), sleeping (-)
-
-    """
-    pass
-
-
-def get_trigger():
-    """"
-    1)Bidargadi et al. (2018) "To Prompt or Not to Prompt? A Microrandomized Trial of Time-Varying Push Notifications to
-     Increase Proximal Engagement With a Mobile Health App"
-    Timing!: "users are more likely to engage with the app within 24 hours when push notifications are sent at mid-day
-     on weekends"
-     - time of the day
-     - day of the week
-
-    2) Akker etal (2015) "Tailored motivational message generation: A model and practical framework for real-time
-     physical activity coaching"
-
-    """
-    pass
+        return effective_prompt
 
 
 def update_patient_stress_level():
-
     """"
     Stress = high arousal and negative valence Markova et al (2019) "arousal-valence emotion space"
     in contrast to
@@ -133,7 +139,6 @@ def update_patient_cognitive_load():
 
 
 def update_patient_arousal():
-
     """"
     Kusserow et al (2013) "Modeling arousal phases in daily living using wearable sensors"
     "participant-specific arousal was frequently estimated during conversations and
@@ -145,7 +150,6 @@ def update_patient_arousal():
 
 
 def update_patient_valence():
-
     """"
     Baglioni  et al (2010) "Sleep and emotions: A focus on insomnia"
     "interaction between sleep and emotional valence, poor sleep quality seems to
@@ -199,8 +203,5 @@ def update_patients_sleep_duration():
     - walking (+)
     """
     pass
-
-
-
 
 #
--- tests/test_enviroments.py
+++ tests/test_enviroments.py
@@ -6,7 +6,7 @@ class PatientEnvironmentTest(unittest.TestCase):
 
     def test_patient_behaviour(self):
 
-        test_patient = Patient(10)
+        test_patient = Patient(10, True)
 
         # test trigger
         self.assertFalse(test_patient.fogg_behaviour(5, 4, False))
@@ -15,6 +15,41 @@ class PatientEnvironmentTest(unittest.TestCase):
         # test threshold
         self.assertFalse(test_patient.fogg_behaviour(5, 2, True))
         self.assertTrue(test_patient.fogg_behaviour(4, 3, True))
+
+    def test_patient_motivation(self):
+        test_patient = Patient(4, True)
+        self.assertIsNotNone(test_patient.number_of_hours_slept_last_night)
+        self.assertIsNotNone(test_patient.sufficient_sleep)
+        self.assertIsNotNone(test_patient.last_activity_score)
+        self.assertIsNotNone(test_patient.valence)
+        self.assertLessEqual(test_patient.get_motivation(), 4)
+        test_patient2 = Patient(4, False)
+        self.assertLessEqual(test_patient2.get_motivation(), 3)
+
+    def test_patient_ability(self):
+        test_patient = Patient(2, False)
+        self.assertIsNotNone(test_patient.location)
+        self.assertIsNotNone(test_patient.activity)
+        self.assertIsNotNone(test_patient.cognitive_load)
+        self.assertIsNotNone(test_patient.arousal)
+        self.assertLessEqual(test_patient.get_ability(), 4)
+
+    def test_trigger(self):
+        test_patient = Patient(2, False)
+        self.assertIsNotNone(test_patient.day_of_the_week)
+        self.assertIsNotNone(test_patient.time_of_the_day)
+        self.assertIsNotNone(test_patient.get_trigger())
+
+        test_patient2 = Patient(2, False)
+        test_patient2.day_of_the_week =6
+        test_patient2.activity = 'stationary'
+        test_patient2.time_of_the_day = 11
+        self.assertTrue(test_patient2.get_trigger())
+
+        test_patient2 = Patient(2, False)
+        test_patient2.day_of_the_week = 6
+        test_patient2.activity = 'sleeping'
+        self.assertFalse(test_patient2.get_trigger())
 
 
 
